52fa335efe5ad5c986ed49310ad335259c663f63
vjlock <lch.dzign@gmail.com>
2014-08-04 12:28:50 +0200

    Insertion home page + footer / css + js

---
+++ application/views/generic/footer.php
@@ -0,0 +1,22 @@
+
+        <!-- FOOTER -->
+        <div class="col-sm-12">
+
+            <p class="text-left"><a href="#">Mentions légales</a> / <a href="#">Contact</a></p>
+
+            <p class="text-right">Partenaires : Pays, Conseil Général</p>
+
+        </div>
+
+        <!-- FIN FOOTER -->
+
+
+
+    </div> <!-- / .container-->
+
+
+<!-- SCRIPT -->
+<script src=""></script>
+
+</body>
+</html>
---
+++ application/views/generic/header.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>GoMobility + PageTitle</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/bootstrap.css"/>
+
+    <script src="<?= base_url(); ?>assets/js/jquery-2.1.1.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+    <div class="container">
+
+        <!-- MENU -->
+        <div class="col-sm-12">
+
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">GoMobility</a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Le projet</a></li>
+                            <li><a href="#">Vos expériences</a></li>
+                            <li><a href="#">Je participe</a></li>
+                        </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+
+        </div>
+
+        <!-- FIN MENU -->
+
+
+
+
+
+
+
+
