1ac19a519de37abc04af0316348a817774d28aed
kevin <kgrabiner75@gmail.com>
2014-08-28 22:18:56 +0200

    correction pour l'ajout dans le crud

--- application/views/admin/pages/v_crud.php
+++ application/views/admin/pages/v_crud.php
@@ -28,6 +28,7 @@
         </ul>
     </div>
 
+
     <table class="table table-bordered ">
         <thead>
             <tr>
@@ -37,6 +38,44 @@
             </tr>
         </thead>
         <tbody>
+
+        <tr ng-repeat="(keyData, data) in datas | filter:{ id: '#' }" ng-class="{'bg-danger': data.dataDelete == 1, 'bg-add': data.dataAdd == 1}">
+            <td>
+                <div>
+                    <span>{{data.id}}</span>
+                </div>
+            </td>
+            <td ng-repeat="(key, value) in tableInfos" ng-show="!$first" ng-click="updateData()" ng-class="{'bg-success': data.dataUpdated[value.Field] == 1, 'bg-danger': data.dataDelete == 1}">
+
+
+
+                <span ng-if="value.Field != 'id'" class="fa fa-pencil" ></span>
+
+                <div class="data" ng-show="!clicked">
+                    <span>{{data[value.Field]|limitTo:15}}</span>
+                    <span ng-if="data[value.Field].length > 15">...</span>
+                </div>
+
+
+                <input type="text" ng-show="clicked" ng-blur="dataUpdated(value.Field, data[value.Field], keyData)" value="{{data[value.Field]}}" size="{{data[value.Field].length > 15 ? 15 : data[value.Field].length}}" ng-model="data[value.Field]" />
+
+
+            </td>
+
+            <td>
+                <button type="button" class="btn btn-danger" ng-click="dataDelete(value.Field, data[value.Field], keyData)" ng-show="!data.dataDelete">
+                    <span class="glyphicon glyphicon-trash"></span>
+                </button>
+                <button type="button" class="btn btn-success" ng-click="dataUndelete(value.Field, data[value.Field], keyData)" ng-show="data.dataDelete">
+                    <span class="glyphicon glyphicon-export"></span>
+                </button>
+            </td>
+            <!--td>
+                <button type="button" class="btn btn-primary">
+                    <span class="glyphicon glyphicon-eye-open"></span>
+                </button>
+            </td-->
+        </tr>
 
             <tr ng-repeat="(keyData, data) in pagedItems[currentPage] | orderBy:sortingOrder:reverse" ng-class="{'bg-danger': data.dataDelete == 1, 'bg-add': data.dataAdd == 1}">
                 <td>
